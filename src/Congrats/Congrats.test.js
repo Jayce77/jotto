104ad741b92a9f576ed5cdd2dac6cd4275989c98
Jayce77 <xjaycex@hotmail.com>
2021-10-23 14:47:19 -0400

    Added GuessedWords component

--- src/Congrats/Congrats.test.js
+++ src/Congrats/Congrats.test.js
@@ -9,20 +9,17 @@ const setUp = (props={}) => {
 test('renders without error', () => {
   const wrapper = setUp();
   const component = findByTestAttr(wrapper, 'component-congrats');
-  console.log(wrapper.debug())
   expect(component.length).toBe(1);
 });
 
 test('renders no text when success is false', () => {
   const wrapper = setUp({ success: false});
   const component = findByTestAttr(wrapper, 'component-congrats');
-  console.log(wrapper.debug())
   expect(component.text()).toBe('');
 });
 
 test('renders non-empty success message when success is true', () => {
   const wrapper = setUp({ success: true});
   const message = findByTestAttr(wrapper, 'congrats-message');
-  console.log(wrapper.debug())
   expect(message.text().length).not.toBe(0);
 });
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,19 @@
+import { checkPropTypes } from 'prop-types';
+
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`)
+}
+
+export const checkProps = (component, conformingProps) => {
+  let propError;
+  const t = console.error;
+  console.error = msg => { throw new Error(msg) };
+  try {
+    checkPropTypes(component.propTypes, conformingProps, 'prop', component.name);
+  } catch (error) {
+    propError = error;
+  }
+ 
+  expect(propError).toBeUndefined();
+  console.error = t;
 }
