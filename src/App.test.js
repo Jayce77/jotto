3d1f70c38f87eaf49927b1b7e1c027cbc8095c4e
Jayce77 <xjaycex@hotmail.com>
2021-10-24 19:34:52 -0400

    Base app complete

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils'
 import App from './App';
 
+// activate global mock to make sure getSecretWord doesn't make network call
+jest.mock('./actions');
+// eslint-disable-next-line import/first
+import { getSecretWord as mockGetSecretWord } from './actions';
+
 const setUp = () => {
-  return shallow(<App />);
+  return mount(<App />);
 }
 
 test('renders without error', () => {
@@ -11,3 +16,24 @@ test('renders without error', () => {
   const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent).toHaveLength(1);
 });
+
+describe('get secret word', () => {
+  beforeEach(() => {
+    // clear the mock calls from previous tests
+    mockGetSecretWord.mockClear();
+  });
+
+  test('get secret word on app mount', () => {
+    const wrapper = setUp();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+
+  test('get secret word does not run on app update', () => {
+    const wrapper = setUp();
+    mockGetSecretWord.mockClear();
+
+    wrapper.setProps()
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
+})
